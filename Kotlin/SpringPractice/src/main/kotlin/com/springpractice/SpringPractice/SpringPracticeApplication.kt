22d05f8153fd9f2afb287e146b9b5fd0ef526219
Ryusei Kurata <amnosmusic13@gmail.com>
2019-03-26 11:17:34 +0900

    [add]Kotlin環境構築

---
+++ Kotlin/SpringPractice/src/main/kotlin/com/springpractice/SpringPractice/SpringPracticeApplication.kt
@@ -0,0 +1,11 @@
+package com.springpractice.SpringPractice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringPracticeApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringPracticeApplication>(*args)
+}
