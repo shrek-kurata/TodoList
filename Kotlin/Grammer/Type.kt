6cdce5b02ea49ec7cb0534995f2eebea75335dd4
Ryusei Kurata <amnosmusic13@gmail.com>
2019-05-01 12:25:59 +0900

    init Gettogeether

---
+++ Kotlin/Grammer/Type.kt
@@ -0,0 +1,21 @@
+var num = 3
+var i = 0
+i = 1
+val a: Bite = 1
+
+var digits = 123
+var longInt = 123L
+val hex = 0x0F
+val bin = 0b0000101
+val dbl = 123.5
+val withE = 123.5e10
+val flt = 123.5f
+
+val c = 'c'
+val n = '\n'
+val u = '\uFF00'
+
+var a: Bite = 1
+var a: Long = 1
+val a: string = 1 
+
