18981297252db9cb03625f4d2e55d990d679e16e
Ryusei Kurata <amnosmusic13@gmail.com>
2019-05-07 15:12:55 +0900

    clean

--- CleanArchitecture/dist/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var express = require("express");
-var app = express();
-var port = 3000;
-app.get('/', function (req, res) { return res.send('Hello World!'); });
-app.listen(port, function () { return console.log("Example app listening on port " + port + "!"); });
-//# sourceMappingURL=index.js.map
