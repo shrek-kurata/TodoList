34d2aeff40f3d5cbf5faaf1815de9809dd633b15
Ryusei Kurata <amnosmusic13@gmail.com>
2019-05-08 02:50:53 +0000

    Sql handler

--- CleanArchitecture/src/app/interfaces/database/sql_handler.go
+++ CleanArchitecture/src/app/interfaces/database/sql_handler.go
@@ -4,3 +4,14 @@ type SqlHandler interface {
 	Execute(string, ...interface{}) (Result, error)
 	Query(string, ...interface{}) (Row, error)
 }
+
+type Result interface {
+	LastInsertId() (int64, error)
+	Rowsaffected() (int64, error)
+}
+
+type Row interface {
+	Scan(...interface{}) error
+	Next() bool
+	Close() bool
+}
