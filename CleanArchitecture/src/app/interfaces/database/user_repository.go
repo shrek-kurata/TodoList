f5c576082dc8f0490a5bd2a26acc3260fbbcb658
Ryusei Kurata <amnosmusic13@gmail.com>
2019-05-08 02:50:34 +0000

    Change import

---
+++ CleanArchitecture/src/app/interfaces/database/user_repository.go
@@ -0,0 +1,14 @@
+package database
+
+import "app/domain"
+
+type UserRepository struct {
+	SqlHandler
+}
+
+func (repo *UserRepository) Store(u domain.User) (id int, err error) {
+	result, err := repo.Execute(
+		"INSERT INTO users (first_name, last_name) VALUES (?,?)", 
+	)
+}
+
