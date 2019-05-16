a4737206e4c3e4b1d807941248de6bf20b89abc6
Ryusei Kurata <amnosmusic13@gmail.com>
2019-05-17 00:18:16 +0900

    a

--- Flutter/gettogether/lib/main.dart
+++ Flutter/gettogether/lib/main.dart
@@ -20,7 +20,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'Gettogether'),
     );
   }
 }
@@ -44,18 +44,6 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -69,43 +57,58 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
+        title: Text(
+          widget.title,
+          style: TextStyle(fontFamily: 'Avenir', fontSize: 20),
         ),
+        centerTitle: false,
+        backgroundColor: Color.fromRGBO(157, 232, 104, 1),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      body: Column(
+children: <Widget>[],
+      )
+    );
+  }
+
+  Widget eventListsContainer() {
+    return Padding(
+      padding: const EdgeInsets.only(top: 30, left: 16, right: 16),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[
+          eventList(),
+          eventList()
+        ],
+      ),
+    );
+  }
+
+  Widget eventList() {
+    return ListView(
+      scrollDirection: Axis.horizontal,
+      children: <Widget>[
+        // あとでAPIで取ってくる
+        Container(
+          width: 160.0,
+          color: Colors.red,
+        ),
+        Container(
+          width: 160.0,
+          color: Colors.blue,
+        ),
+        Container(
+          width: 160.0,
+          color: Colors.green,
+        ),
+        Container(
+          width: 160.0,
+          color: Colors.yellow,
+        ),
+        Container(
+          width: 160.0,
+          color: Colors.orange,
+        ),
+      ],
     );
   }
 }
