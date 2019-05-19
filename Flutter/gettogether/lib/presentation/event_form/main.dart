bba6d1e94045f4c46db24ba79aac403ee23556ce
Ryusei Kurata <amnosmusic13@gmail.com>
2019-05-19 14:57:57 +0900

    banner

---
+++ Flutter/gettogether/lib/presentation/event_form/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class EventForm extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: EventFormPage(),
+    );
+  }
+}
+
+class EventFormPage extends StatefulWidget {
+  EventFormPage() : super();
+
+  @override
+  _EventFormPageState createState() => _EventFormPageState();
+}
+
+class _EventFormPageState extends State<EventFormPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(child: Column(children: <Widget>[
+        Expanded(child: Container(color: Color.fromRGBO(157, 232, 104, 1)), flex: 1),
+        Expanded(child: Container(color: Colors.red),flex: 2,)
+      ],),),
+    );
+  }
+}
