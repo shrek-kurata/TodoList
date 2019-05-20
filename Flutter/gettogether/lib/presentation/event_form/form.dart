ddce9374d2f4ea769b9fbd6c51e25954dc1e092a
Ryusei Kurata <amnosmusic13@gmail.com>
2019-05-20 14:10:28 +0900

    form

---
+++ Flutter/gettogether/lib/presentation/event_form/form.dart
@@ -0,0 +1,97 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class EventForm {
+  Widget view() => Container(
+      width: 330,
+      height: 430,
+      padding: EdgeInsets.symmetric(vertical: 20, horizontal: 24),
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(8.0), color: Colors.white),
+      child: _itemContainer());
+
+  Widget _itemContainer() => Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: <Widget>[
+          _bigForm('どのスポーツをしたい？'),
+          _smallFormContainer(['いつ遊ぶ？', 'どのくらい遊ぼうか？']),
+          _smallFormContainer(['何人で遊びたい？', '予算はどのくらい？']),
+          _bigForm('どこで遊ぶ？'),
+          _button()
+        ],
+      );
+
+  /// SmallForm
+
+  Widget _smallFormContainer(List<String> titles) => Row(
+        mainAxisSize: MainAxisSize.max,
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[_smallForm(titles[0]), _smallForm(titles[1])],
+      );
+
+  Widget _smallForm(String title) => Column(
+        mainAxisSize: MainAxisSize.max,
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[
+          Container(
+              alignment: Alignment.centerLeft,
+              child: Text(
+                title,
+                style: TextStyle(fontSize: 10),
+              )),
+          _smallLabel()
+        ],
+      );
+
+  Widget _smallLabel() => Container(
+        decoration: BoxDecoration(
+            border: Border.all(
+                color: Color.fromRGBO(230, 234, 234, 1),
+                width: 2,
+                style: BorderStyle.solid),
+            borderRadius: BorderRadius.circular(6.0)),
+        width: 130,
+        height: 45,
+      );
+
+  // BigForm
+
+  Widget _bigForm(String title) => Column(
+        mainAxisSize: MainAxisSize.max,
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: <Widget>[
+          Container(
+              alignment: Alignment.centerLeft,
+              child: Text(title,
+                  textAlign: TextAlign.start, style: TextStyle(fontSize: 10))),
+          _bigLabel()
+        ],
+      );
+
+  Widget _bigLabel() => Container(
+        decoration: BoxDecoration(
+            border: Border.all(
+                color: Color.fromRGBO(230, 234, 234, 1),
+                width: 2,
+                style: BorderStyle.solid),
+            borderRadius: BorderRadius.circular(6.0)),
+        width: 280,
+        height: 50,
+      );
+
+  Widget _button() => RaisedButton(
+        onPressed: () {},
+        padding: EdgeInsets.symmetric(vertical: 17, horizontal: 33.0),
+        color: Color.fromRGBO(157, 232, 104, 1),
+        textColor: Colors.white,
+        child: Center(
+          child: Text(
+            'Gettogether',
+            style: TextStyle(fontFamily: 'Avenir', fontSize: 15),
+          ),
+        ),
+        shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.all(Radius.circular(20.0))),
+      );
+}
--- Flutter/gettogether/lib/presentation/event_form/main.dart
+++ Flutter/gettogether/lib/presentation/event_form/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:gettogether/presentation/event_form/form.dart';
 
 class EventFormPage extends StatefulWidget {
   EventFormPage() : super();
@@ -13,63 +14,27 @@ class _EventFormPageState extends State<EventFormPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Stack(children: <Widget>[
-      SafeArea(
-        child: Column(
-          children: <Widget>[
-            Expanded(
-                child: Container(color: Color.fromRGBO(157, 232, 104, 1)),
-                flex: 1),
-            Expanded(
-              child: Container(color: Colors.white),
-              flex: 2,
-            )
-          ],
+        body: Stack(
+      children: <Widget>[
+        Container(
+          child: Column(
+            children: <Widget>[
+              Expanded(
+                  child: Container(color: Color.fromRGBO(157, 232, 104, 1)),
+                  flex: 1),
+              Expanded(
+                child: Container(color: Colors.white),
+                flex: 2,
+              )
+            ],
+          ),
         ),
-      ),
-      Container(
-        padding: EdgeInsets.only(top: 104, right: 24, left: 24, bottom: 256),
-        child: Column(
-          children: <Widget>[
-            Container(
-              alignment: Alignment.topLeft,
-              child: Text('一緒にスポーツする人を見つけよう',
-                  style: TextStyle(
-                      fontSize: 15, fontFamily: 'Hiragino Kaku Gothic ProN')),
-            ),
-            Container(
-              decoration:
-                  BoxDecoration(borderRadius: BorderRadius.circular(8.0)),
-              child: Column(
-                mainAxisSize: MainAxisSize.max,
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: <Widget>[
-                  _bigForm('どのスポーツをしたい？'),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    mainAxisSize: MainAxisSize.max,
-                    children: <Widget>[
-                      _smallForm('いつ遊ぶ？'),
-                      _smallForm('どのくらい遊ぼうか？')
-                    ],
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    mainAxisSize: MainAxisSize.max,
-                    children: <Widget>[
-                      _smallForm('何人で遊びたい？'),
-                      _smallForm('予算はどのくらい？')
-                    ],
-                  ),
-                  _bigForm('場所を教えて！')
-                ],
-              ),
-            ),
-          ],
-        ),
-      )
-    ]));
+        Center(child: EventForm().view())
+      ],
+      fit: StackFit.expand,
+    ));
   }
+
 
   Widget _bigLabel() => Container(
         decoration: BoxDecoration(
@@ -94,6 +59,7 @@ class _EventFormPageState extends State<EventFormPage> {
   Widget _bigForm(String title) => Column(
         children: <Widget>[
           Container(
+            color: Colors.black,
             alignment: Alignment.topLeft,
             child: Text(
               title,
