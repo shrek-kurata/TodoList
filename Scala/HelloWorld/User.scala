53c8e542f18208fa04f72546585a3259924cd759
Ryusei Kurata <amnosmusic13@gmail.com>
2019-06-30 13:44:03 +0900

    learn

--- Scala/HelloWorld/User.scala
+++ Scala/HelloWorld/User.scala
@@ -3,3 +3,17 @@ class User(val name: String, val age: Int)
 object User {
     def printUser(user: User) = println(user.name + "" + user.age)
 }
+
+object TypeParamSample {
+    class TypeParam[T](val t: T) {
+        def get: T = this.t
+    }
+
+    def main(args: Array[String]): Unit = {
+        val stringTypeParam = new TypeParam[String]("test")
+        println(stringTypeParam.get)
+
+        val intTypeParam = new TypeParam[Int](1)
+        println(intTypeParam.get)
+    }
+}
